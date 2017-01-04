28b42d86b7559a9fd2e27bbed81fa3874f32e95b
josdem <joseluis.delacruz@gmail.com>
2017-01-04 10:52:25 -0600

    Adding application properties to the project

--- src/main/groovy/com/jos/dem/vetlog/controller/VetlogController.groovy
+++ src/main/groovy/com/jos/dem/vetlog/controller/VetlogController.groovy
@@ -2,15 +2,24 @@ package com.jos.dem.vetlog.controller
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 
 import com.jos.dem.vetlog.service.VetlogService
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 @Controller
-class VetlogController {
+class VetlogController {  
+  Logger log = LoggerFactory.getLogger(this.class)
+
+  @Value('${message}')
+  String message;
   
   @RequestMapping("/")
   String index(){
+  	log.info "message is: ${message}"
     'index'
   }
 }
