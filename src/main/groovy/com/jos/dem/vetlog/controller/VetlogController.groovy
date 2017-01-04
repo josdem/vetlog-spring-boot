ab671e6e2437dbc9eaf633848871d585f46318bb
josdem <joseluis.delacruz@gmail.com>
2017-01-04 10:23:49 -0600

    Adding index page

--- src/main/groovy/com/jos/dem/vetlog/controller/VetlogController.groovy
+++ src/main/groovy/com/jos/dem/vetlog/controller/VetlogController.groovy
@@ -1,19 +1,16 @@
 package com.jos.dem.vetlog.controller
 
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
 
 import com.jos.dem.vetlog.service.VetlogService
 
-@RestController
+@Controller
 class VetlogController {
-
-  @Autowired
-  VetlogService vetlogService
-
+  
   @RequestMapping("/")
   String index(){
-    vetlogService.sendRegistration()
+    'index'
   }
 }
