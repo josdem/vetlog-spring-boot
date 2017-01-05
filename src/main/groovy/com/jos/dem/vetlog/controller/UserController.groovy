50e5706d13cc3f0834a53c01617959305f9993fe
josdem <joseluis.delacruz@gmail.com>
2017-01-04 22:03:28 -0600

    #2 Adding link to create user

--- src/main/groovy/com/jos/dem/vetlog/controller/UserController.groovy
+++ src/main/groovy/com/jos/dem/vetlog/controller/UserController.groovy
@@ -1,13 +1,23 @@
 package com.jos.dem.vetlog.controller
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.servlet.ModelAndView
 import org.springframework.stereotype.Controller
 
-@controller
+import com.jos.dem.vetlog.command.UserCommand
+
+@Controller
+@RequestMapping("/users")
 class UserController {
 
-  def create(){
+  @RequestMapping(method = GET, value = "/create")
+  String create(){
+    def modelAndView = new ModelAndView('create')
     def userCommand = new UserCommand()
-    userCommand
+    modelAndView.addObject('userCommand', userCommand)
+    'user/create'
   }
   
 }
