c356ce5f5d57be7f4edf1dbf4c68db1ce732fff2
josdem <joseluis.delacruz@gmail.com>
2017-01-05 19:38:45 -0600

    #2 Completing the fields for registration

--- src/main/groovy/com/jos/dem/vetlog/command/UserCommand.groovy
+++ src/main/groovy/com/jos/dem/vetlog/command/UserCommand.groovy
@@ -3,7 +3,8 @@ package com.jos.dem.vetlog.command
 class UserCommand implements Command {
   String username
   String password
+  String passwordConfirmation
   String name
-  String lastName
+  String lastname
   String email
 }
