150238b9a06eccb13be2c54ff6dcd6f559c4217a
josdem <joseluis.delacruz@gmail.com>
2017-01-06 16:15:58 -0600

    #4 Adding valitation annotations

--- src/main/groovy/com/jos/dem/vetlog/command/UserCommand.groovy
+++ src/main/groovy/com/jos/dem/vetlog/command/UserCommand.groovy
@@ -1,10 +1,25 @@
 package com.jos.dem.vetlog.command
 
+import org.hibernate.validator.constraints.NotEmpty
+import org.hibernate.validator.constraints.Email
+
 class UserCommand implements Command {
+
+  @NotEmpty("Username can not be empty")
   String username
+
+  @NotEmpty("Password can not be empty")
   String password
+
+  @NotEmpty("Password confirmation can not be empty")
   String passwordConfirmation
+
+  @NotEmpty("Name can not be empty")
   String name
+
+  @NotEmpty("Lastname can not be empty")
   String lastname
+
+  @Email
   String email
 }
