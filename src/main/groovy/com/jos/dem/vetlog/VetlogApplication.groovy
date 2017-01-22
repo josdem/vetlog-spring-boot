eb9812ff09178824e825308ebfae6a7f89e2af5f
josdem <joseluis.delacruz@gmail.com>
2017-01-21 20:07:01 -0600

    #7 Deleting unused dependency

--- src/main/groovy/com/jos/dem/vetlog/VetlogApplication.groovy
+++ src/main/groovy/com/jos/dem/vetlog/VetlogApplication.groovy
@@ -2,10 +2,9 @@ package com.jos.dem.vetlog
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.web.SpringBootServletInitializer
 
 @SpringBootApplication
-class VetlogApplication extends SpringBootServletInitializer{
+class VetlogApplication {
 
 	static void main(String[] args) {
 		SpringApplication.run VetlogApplication, args
