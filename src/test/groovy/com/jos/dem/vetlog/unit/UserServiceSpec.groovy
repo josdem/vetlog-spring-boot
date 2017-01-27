fea79b7863d1c6ebea39293cd5919f0b0388354c
josdem <joseluis.delacruz@gmail.com>
2017-01-26 20:50:54 -0600

    #11 Adding test to save an user

--- src/test/groovy/com/jos/dem/vetlog/unit/UserServiceSpec.groovy
+++ src/test/groovy/com/jos/dem/vetlog/unit/UserServiceSpec.groovy
@@ -5,15 +5,21 @@ import spock.lang.Specification
 import com.jos.dem.vetlog.service.UserService
 import com.jos.dem.vetlog.service.impl.UserServiceImpl
 import com.jos.dem.vetlog.repository.UserRepository
+import com.jos.dem.vetlog.binder.UserBinder
+import com.jos.dem.vetlog.model.User
+import com.jos.dem.vetlog.command.Command
+import com.jos.dem.vetlog.command.UserCommand
 
 class UserServiceSpec extends Specification {
 
   UserService userService = new UserServiceImpl()
 
   UserRepository userRepository = Mock(UserRepository)
+  UserBinder userBinder = new UserBinder()
 
   def setup(){
     userService.userRepository = userRepository
+    userService.userBinder = userBinder
   }
 
   void "should find an user by username"(){
@@ -32,6 +38,17 @@ class UserServiceSpec extends Specification {
       userService.getByEmail(email)
     then:"We expect repository delegation"
     1 * userRepository.findByEmail(email)
+  }
+
+  void "should save an user"(){
+    given:"A command"
+      Command command = new UserCommand(username:'josdem',password:'password', passwordConfirmation:'password', name:'josdem',lastname:'lastname',email:'josdem@email.com')
+    and:"A user"
+      User user = new User()
+    when:"We save an user"
+      userService.save(command)
+    then:"We expect repository delegation"
+    1 * userRepository.save(_ as User)
   }
 
 }
