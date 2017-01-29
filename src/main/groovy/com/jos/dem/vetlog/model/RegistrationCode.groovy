31b81e2822599f379e82094a7f0392d01a2e3c65
josdem <joseluis.delacruz@gmail.com>
2017-01-29 11:50:14 -0600

    #10 Adding id to the registration code

--- src/main/groovy/com/jos/dem/vetlog/model/RegistrationCode.groovy
+++ src/main/groovy/com/jos/dem/vetlog/model/RegistrationCode.groovy
@@ -1,13 +1,20 @@
 package com.jos.dem.vetlog.model
 
+import static javax.persistence.GenerationType.AUTO
+
+import javax.persistence.Id
 import javax.persistence.Entity
 import javax.persistence.Column
+import javax.persistence.GeneratedValue
 
 import com.jos.dem.vetlog.enums.RegistrationCodeStatus
 
 @Entity
 class RegistrationCode {
 
+  @Id
+  @GeneratedValue(strategy=AUTO)
+  Long id
   @Column(nullable = false)
   String email
   @Column(nullable = false)
