85f1973be59398c8236f23bd6718a731ef1bb506
josdem <joseluis.delacruz@gmail.com>
2017-01-31 09:47:25 -0600

    #14 Adding test to validate date

---
+++ src/test/groovy/com/jos/dem/vetlog/unit/RegistrationCodeSpec.groovy
@@ -0,0 +1,18 @@
+package com.jos.dem.vetlog.unit
+
+import spock.lang.Specification
+
+import com.jos.dem.vetlog.model.RegistrationCode
+
+class RegistrationCodeSpec extends Specification {
+
+  RegistrationCode registrationCode = new RegistrationCode()
+
+  void "should create a registration code with creation date"(){
+    given:"A date"
+      Date now = new Date()
+    expect:"Date is not null and in the past"
+      registrationCode.dateCreated.getTime() - now.getTime() < 10000
+  }
+	
+}
