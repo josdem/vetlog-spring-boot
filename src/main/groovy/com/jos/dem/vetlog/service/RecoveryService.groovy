4df30e1da9f59019dfc1a1b8f1e1ebf40e86c541
josdem <joseluis.delacruz@gmail.com>
2017-02-05 15:14:33 -0600

    #16 Adding confirmAccountForToken method

--- src/main/groovy/com/jos/dem/vetlog/service/RecoveryService.groovy
+++ src/main/groovy/com/jos/dem/vetlog/service/RecoveryService.groovy
@@ -1,5 +1,8 @@
 package com.jos.dem.vetlog.service
 
+import com.jos.dem.vetlog.model.User
+
 interface RecoveryService {
   void sendConfirmationAccountToken(String email)
+  User confirmAccountForToken(String token)
 }
