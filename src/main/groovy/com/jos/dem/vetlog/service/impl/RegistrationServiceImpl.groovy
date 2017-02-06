c17eede50ec8b3a5be6d8a662339a6693d9c171c
josdem <joseluis.delacruz@gmail.com>
2017-02-06 11:53:48 -0600

    #18 Adding vetlog exception when token not found

--- src/main/groovy/com/jos/dem/vetlog/service/impl/RegistrationServiceImpl.groovy
+++ src/main/groovy/com/jos/dem/vetlog/service/impl/RegistrationServiceImpl.groovy
@@ -4,17 +4,22 @@ import org.springframework.stereotype.Service
 import org.springframework.beans.factory.annotation.Autowired
 
 import com.jos.dem.vetlog.service.RegistrationService
+import com.jos.dem.vetlog.service.LocaleService
 import com.jos.dem.vetlog.model.RegistrationCode
 import com.jos.dem.vetlog.repository.RegistrationCodeRepository
+import com.jos.dem.vetlog.exception.VetlogException
 
 @Service
 class RegistrationServiceImpl implements RegistrationService {
 
   @Autowired
+  LocaleService localeService
+  @Autowired
   RegistrationCodeRepository repository
 
   String findEmailByToken(String token){
     RegistrationCode registrationCode = repository.findByToken(token)
+    if(!registrationCode) throw new VetlogException(localeService.getMessage('exception.token.not.found'))
     registrationCode.email
   }
 
