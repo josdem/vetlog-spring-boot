1af20ebcd5e547c0c0995f7bfc384e98a7beeefc
josdem <joseluis.delacruz@gmail.com>
2017-02-06 12:34:15 -0600

    #18 Adding should not find email by token since token does not exist test

--- src/test/groovy/com/jos/dem/vetlog/unit/RegistrationServiceSpec.groovy
+++ src/test/groovy/com/jos/dem/vetlog/unit/RegistrationServiceSpec.groovy
@@ -4,17 +4,21 @@ import spock.lang.Specification
 
 import com.jos.dem.vetlog.service.RegistrationService
 import com.jos.dem.vetlog.service.impl.RegistrationServiceImpl
+import com.jos.dem.vetlog.service.LocaleService
 import com.jos.dem.vetlog.repository.RegistrationCodeRepository
 import com.jos.dem.vetlog.model.RegistrationCode
+import com.jos.dem.vetlog.exception.VetlogException
 
 class RegistrationServiceSpec extends Specification {
 
   RegistrationService registrationService = new RegistrationServiceImpl()
 
   RegistrationCodeRepository repository = Mock(RegistrationCodeRepository)
+  LocaleService localeService = Mock(LocaleService)
 
   def setup(){
     registrationService.repository = repository
+    registrationService.localeService = localeService
   }
 
   void "should find email by token"(){
@@ -28,4 +32,15 @@ class RegistrationServiceSpec extends Specification {
     then:"We expect email"
      'josdem@email.com' == result
   }
+
+  void "should not find email by token since token does not exist"(){
+    given:"An token"
+      String token = 'token'
+    when:"We find registration code by token"
+     localeService.getMessage('exception.token.not.found') >> 'Token not found'
+     String result = registrationService.findEmailByToken(token)
+    then:"We expect exception"
+     thrown VetlogException
+  }
+
 }
