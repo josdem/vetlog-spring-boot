2a812427cf1b838e51c4a803cfedd9591e537ef0
josdem <joseluis.delacruz@gmail.com>
2017-02-06 17:12:20 -0600

    #17 Adding forgot password page

--- src/main/groovy/com/jos/dem/vetlog/controller/RecoveryController.groovy
+++ src/main/groovy/com/jos/dem/vetlog/controller/RecoveryController.groovy
@@ -4,6 +4,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.servlet.ModelAndView
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 
@@ -27,5 +29,15 @@ class RecoveryController {
     recoveryService.confirmAccountForToken(token)
     'login/login'
 	}
+
+  @RequestMapping("/password")
+  ModelAndView login(@RequestParam Optional<String> error){
+  	log.info "Calling recovery password"
+    ModelAndView modelAndView = new ModelAndView('recovery/recoveryPassword')
+    if(error.isPresent()){
+      modelAndView.addObject('error', localeService.getMessage('recovery.error'))
+    }
+    modelAndView
+  }
 
 }
