e8614bb2eabd6f9360c9b12845528e375b058a15
josdem <joseluis.delacruz@gmail.com>
2017-02-11 13:41:27 -0600

    #19 Adding constraints

--- src/main/groovy/com/jos/dem/vetlog/command/RecoveryPasswordCommand.groovy
+++ src/main/groovy/com/jos/dem/vetlog/command/RecoveryPasswordCommand.groovy
@@ -1,9 +1,13 @@
 package com.jos.dem.vetlog.command
 
+import javax.validation.constraints.Size
+import javax.validation.constraints.NotNull
 import org.hibernate.validator.constraints.Email
 
 class RecoveryPasswordCommand implements Command {
 
+  @NotNull
+  @Size(min=6)
   @Email
   String email
 
