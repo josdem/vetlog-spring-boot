a17e532a0ccb701ddde3a7789c36503c156af7cc
josdem <joseluis.delacruz@gmail.com>
2017-02-12 17:54:38 -0600

    #22 Adding encoder import

--- src/main/groovy/com/jos/dem/vetlog/service/impl/RecoveryServiceImpl.groovy
+++ src/main/groovy/com/jos/dem/vetlog/service/impl/RecoveryServiceImpl.groovy
@@ -3,6 +3,7 @@ package com.jos.dem.vetlog.service.impl
 import org.springframework.stereotype.Service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 import com.jos.dem.vetlog.service.RecoveryService
 import com.jos.dem.vetlog.service.RegistrationService
