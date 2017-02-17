12af937942a9c14429eccd519cad43a9f9100d66
josdem <joseluis.delacruz@gmail.com>
2017-02-17 02:43:08 -0600

    #24 Loading development environment

--- src/main/groovy/com/jos/dem/vetlog/config/Bootstrap.groovy
+++ src/main/groovy/com/jos/dem/vetlog/config/Bootstrap.groovy
@@ -1,15 +1,22 @@
 package com.jos.dem.vetlog.config
 
 import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
+import org.springframework.core.env.Environment
 
 @Component
 class Bootstrap implements ApplicationListener<ApplicationReadyEvent> {
 
+  @Autowired
+  Environment environment
+
   @Override
   void onApplicationEvent(final ApplicationReadyEvent event) {
-    println 'Hello World!'
+    if(environment.activeProfiles[0] == 'development'){
+      println "Loading development environment"
+    }
   }
 
 }
