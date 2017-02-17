8723fd4e97c02cca2bd376dd02304396dd997e73
josdem <joseluis.delacruz@gmail.com>
2017-02-17 03:01:30 -0600

    #24 Creating default user

--- src/main/groovy/com/jos/dem/vetlog/config/Bootstrap.groovy
+++ src/main/groovy/com/jos/dem/vetlog/config/Bootstrap.groovy
@@ -1,21 +1,48 @@
 package com.jos.dem.vetlog.config
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 import org.springframework.core.env.Environment
 
+import com.jos.dem.vetlog.model.User
+import com.jos.dem.vetlog.model.Role
+import com.jos.dem.vetlog.repository.UserRepository
+
 @Component
 class Bootstrap implements ApplicationListener<ApplicationReadyEvent> {
 
   @Autowired
   Environment environment
+  @Autowired
+  UserRepository userRepository
 
   @Override
   void onApplicationEvent(final ApplicationReadyEvent event) {
     if(environment.activeProfiles[0] == 'development'){
       println "Loading development environment"
+      createDefaultUsers()
+    }
+  }
+
+  void createDefaultUsers(){
+    createUserWithRole('softtekian', '12345678', 'softtekian@softtek.com', Role.USER)
+  }
+
+  void createUserWithRole(String username, String password, String email, Role authority) {
+    if(!userRepository.findByUsername(username)){
+      User user = new User(
+        username:username,
+        password:new BCryptPasswordEncoder().encode(password),
+        email:email,
+        role:authority,
+        firstname:username,
+        lastname:username,
+        enabled:true
+      )
+      userRepository.save(user)
     }
   }
 
