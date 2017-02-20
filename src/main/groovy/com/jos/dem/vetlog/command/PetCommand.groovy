6cf314ccddbcae7fba3be2dd75661c0e27b9a0bc
josdem <joseluis.delacruz@gmail.com>
2017-02-19 19:07:15 -0600

    #26 Adding pet command

---
+++ src/main/groovy/com/jos/dem/vetlog/command/PetCommand.groovy
@@ -0,0 +1,36 @@
+package com.jos.dem.vetlog.command
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+import javax.validation.constraints.Min
+import javax.validation.constraints.Max
+
+class PetCommand implements Command {
+
+  @NotNull
+  @Size(min=6, max=50)
+  String name
+
+  @NotNull
+  @Min(0)
+  @Max(250)
+  Integer age
+
+  @NotNull
+  Boolean dewormed = true
+
+  @NotNull
+  Boolean sterilized = true
+
+  @NotNull
+  Boolean vaccinated = true
+
+  @NotNull
+  @Size(min=6, max=50)
+  String breed
+
+  @NotNull
+  @Size(min=6, max=50)
+  String type
+
+}
