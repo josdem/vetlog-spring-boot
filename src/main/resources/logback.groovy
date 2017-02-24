133808e7fa453b261ebac9d135a561e9a2054df1
josdem <joseluis.delacruz@gmail.com>
2017-02-24 15:43:41 -0600

    #27 Changing to the console

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -5,15 +5,12 @@ import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import static ch.qos.logback.classic.Level.DEBUG
 import static ch.qos.logback.classic.Level.INFO
 
-appender("ROLLING", RollingFileAppender) {
+appender("STDOUT", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
     pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{5} Groovy - %msg%n"
-  }
-  rollingPolicy(TimeBasedRollingPolicy){
-    FileNamePattern = "vetlog-spring-boot-%d{yyyy-MM}.log"
   }
 }
 
 logger("com.jos.dem.vetlog", INFO)
 logger('org.springframework', WARN)
-root(WARN, ["ROLLING"])
+root(WARN, ["STDOUT"])
