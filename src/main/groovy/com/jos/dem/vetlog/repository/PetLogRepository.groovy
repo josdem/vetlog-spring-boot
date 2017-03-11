5a73ef6da984a4a2f23129e8d4850da056b22870
josdem <joseluis.delacruz@gmail.com>
2017-03-11 13:49:58 -0600

    #38 Adding pet log repository

---
+++ src/main/groovy/com/jos/dem/vetlog/repository/PetLogRepository.groovy
@@ -0,0 +1,11 @@
+package com.jos.dem.vetlog.repository
+
+import com.jos.dem.vetlog.model.PetLog
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PetLogRepository extends JpaRepository<PetLog,Long> {
+
+  PetLog save(PetLog petLog)
+
+}
+
