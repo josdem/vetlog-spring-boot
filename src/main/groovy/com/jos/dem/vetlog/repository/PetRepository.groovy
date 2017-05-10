8011bb8f6e98a79e23450a444e26feff8fb25062
josdem <joseluis.delacruz@gmail.com>
2017-05-09 19:07:03 -0500

    #50 Adding pet by status in the repository

--- src/main/groovy/com/jos/dem/vetlog/repository/PetRepository.groovy
+++ src/main/groovy/com/jos/dem/vetlog/repository/PetRepository.groovy
@@ -2,11 +2,13 @@ package com.jos.dem.vetlog.repository
 
 import com.jos.dem.vetlog.model.Pet
 import com.jos.dem.vetlog.model.User
+import com.jos.dem.vetlog.enums.PetStatus
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface PetRepository extends JpaRepository<Pet,Long> {
   Pet save(Pet pet)
-  List<Pet> findAllByUser(User user)
   Pet findByUuid(String uuid)
+  List<Pet> findAllByUser(User user)
+  List<Pet> findAllByStatus(PetStatus status)
 }
 
