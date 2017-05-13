fa427dff49e2f3916dde6dda3484eeae7ce8bccc
josdem <joseluis.delacruz@gmail.com>
2017-05-13 18:03:43 -0500

    #53 Fixing test with rest service implementation

--- src/test/groovy/com/jos/dem/vetlog/unit/TelephoneServiceSpec.groovy
+++ src/test/groovy/com/jos/dem/vetlog/unit/TelephoneServiceSpec.groovy
@@ -6,9 +6,11 @@ import com.jos.dem.vetlog.command.Command
 import com.jos.dem.vetlog.command.TelephoneCommand
 import com.jos.dem.vetlog.enums.PetStatus
 import com.jos.dem.vetlog.service.PetService
+import com.jos.dem.vetlog.service.RestService
 import com.jos.dem.vetlog.service.TelephoneService
 import com.jos.dem.vetlog.service.impl.TelephoneServiceImpl
 import com.jos.dem.vetlog.repository.PetRepository
+import com.jos.dem.vetlog.repository.UserRepository
 
 import spock.lang.Specification
 
@@ -18,25 +20,33 @@ class TelephoneServiceSpec extends Specification{
 
   PetService petService = Mock(PetService)
   PetRepository petRepository = Mock(PetRepository)
+  UserRepository userRepository = Mock(UserRepository)
+  RestService restService = Mock(RestService)
 
   def setup(){
     service.petService = petService
     service.petRepository = petRepository
+    service.userRepository = userRepository
+    service.restService = restService
   }
 
   void "should save"(){
     given:"A command"
-      Command command = new TelephoneCommand(uuid:'uuid')
-    and:"A user"
-      User user = new User()
+      Command command = new TelephoneCommand(uuid:'uuid', mobile:'5516827055')
+    and:"A owner and adopter"
+      User owner = new User(email:'josdem@email.com')
+      User adopter = new User(email:'estrella@email.com', mobile:'5500012345')
     and:"A pet"
-      Pet pet = new Pet(name:'Astra Caliope')
+      Pet pet = new Pet(name:'Astra Caliope', user:owner, adopter:adopter)
     when:"We save adoption"
       petService.getPetByUuid('uuid') >> pet
-      service.save(command, user)
+      service.save(command, adopter)
     then:"I expect adoption"
     1 * petRepository.save(pet)
+    1 * userRepository.save(adopter)
+    adopter.mobile == command.mobile
     pet.status == PetStatus.ADOPTED
-    pet.adopter == user
+    pet.adopter == adopter
+    restService.sendCommand(_ as Command)
   }
 }
