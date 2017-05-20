c38516e0b23a9d20bec7bb4c806ec5f3be33d3a1
josdem <joseluis.delacruz@gmail.com>
2017-05-20 15:48:36 -0500

    #63 Adding fill model and view

--- src/main/groovy/com/jos/dem/vetlog/controller/PetController.groovy
+++ src/main/groovy/com/jos/dem/vetlog/controller/PetController.groovy
@@ -68,13 +68,8 @@ class PetController {
   @RequestMapping(method = GET, value = "/create")
   ModelAndView create(@RequestParam (value="type", required=false) String type){
     ModelAndView modelAndView = new ModelAndView('pet/create')
-    if(!type){
-      type = 'Dog'
-    }
-    def breeds = breedService.getBreedsByType(PetType.getPetTypeByValue(type))
-    Command petCommand = new PetCommand(type:PetType.getPetTypeByValue(type))
+    Command petCommand = new PetCommand()
     modelAndView.addObject('petCommand', petCommand)
-    modelAndView.addObject('breeds', breeds)
     fillModelAndView(modelAndView)
   }
 
@@ -99,6 +94,7 @@ class PetController {
   }
 
   ModelAndView fillModelAndView(ModelAndView modelAndView){
+    modelAndView.addObject('breeds', breedService.getBreedsByType(PetType.DOG))
     modelAndView.addObject('breedsByTypeUrl', breedsByTypeUrl)
     modelAndView
   }
