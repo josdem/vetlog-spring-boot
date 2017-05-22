361f16fcff1ed2cefbf536fd99373d3188695bf5
josdem <joseluis.delacruz@gmail.com>
2017-05-21 19:15:58 -0500

    #68 Adding Apache 2.0 licence

--- src/test/groovy/com/jos/dem/vetlog/unit/RegistrationCodeSpec.groovy
+++ src/test/groovy/com/jos/dem/vetlog/unit/RegistrationCodeSpec.groovy
@@ -1,3 +1,19 @@
+/*
+Copyright 2017 José Luis De la Cruz Morales joseluis.delacruz@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.jos.dem.vetlog.unit
 
 import spock.lang.Specification
@@ -20,9 +36,9 @@ class RegistrationCodeSpec extends Specification {
 
   void "should invalidate a token"(){
     when:"We invalidate token"
-      registrationCode.status = RegistrationCodeStatus.INVALID   
+      registrationCode.status = RegistrationCodeStatus.INVALID
     then:"Is not valid"
-      registrationCode.isValid() == false  
+      registrationCode.isValid() == false
   }
-	
+
 }
