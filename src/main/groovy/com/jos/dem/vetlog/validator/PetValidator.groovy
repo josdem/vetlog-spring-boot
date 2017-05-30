2f576b791ff11c0ff70c05657bfdbec8843d349a
josdem <joseluis.delacruz@gmail.com>
2017-05-29 20:14:27 -0500

    #73 Changing error message

--- src/main/groovy/com/jos/dem/vetlog/validator/PetValidator.groovy
+++ src/main/groovy/com/jos/dem/vetlog/validator/PetValidator.groovy
@@ -18,12 +18,11 @@ package com.jos.dem.vetlog.validator
 
 import org.springframework.validation.Validator
 import org.springframework.validation.Errors
-import org.springframework.stereotype.Component
 import org.springframework.beans.factory.annotation.Autowired
 
 import com.jos.dem.vetlog.command.PetCommand
+import com.jos.dem.vetlog.service.LocaleService
 
-@Component
 class PetValidator implements Validator {
 
   @Override
@@ -40,9 +39,8 @@ class PetValidator implements Validator {
   private void validateBirthdate(Errors errors, PetCommand petCommand){
     Date birthDate = Date.parse('MM/dd/yyyy', petCommand.birthDate)
     if(birthDate.compareTo(new Date()) > 0){
-      errors.rejectValue('birthDate', 'error.birthDate', localeService.getMessage('pet.validation.birthdate.past'))
-
+      errors.rejectValue('birthDate', 'error.birthDate', 'Birthdate must be in the past')
     }
-
   }
+
 }
