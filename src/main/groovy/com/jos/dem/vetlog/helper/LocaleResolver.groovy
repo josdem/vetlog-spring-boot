79bea61c68151d1435bfc0002414437774996fd3
josdem <joseluis.delacruz@gmail.com>
2017-06-19 20:25:01 -0500

    #77 Adding spanish as language

--- src/main/groovy/com/jos/dem/vetlog/helper/LocaleResolver.groovy
+++ src/main/groovy/com/jos/dem/vetlog/helper/LocaleResolver.groovy
@@ -21,17 +21,23 @@ import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver
 
 import javax.servlet.http.HttpServletRequest
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 @Component
 class LocaleResolver extends AcceptHeaderLocaleResolver{
 
   private static final List<Locale> LOCALES = [new Locale("en"), new Locale("es")]
 
+  Logger log = LoggerFactory.getLogger(this.class)
+
   @Override
   Locale resolveLocale(HttpServletRequest request) {
+    log.info "Accept Language: ${request.getHeader('Accept-Language')}"
     if (!request.getHeader('Accept-Language')) {
       return Locale.getDefault()
     }
-    List<Locale.LanguageRange> list = Locale.LanguageRange.parse(request.getHeader("Accept-Language"))
+    List<Locale.LanguageRange> list = Locale.LanguageRange.parse(request.getHeader('Accept-Language'))
     Locale locale = Locale.lookup(list, LOCALES)
     return locale
   }
