295675329d68609ce8d08cc77dbdf8cdd41387bd
josdem <joseluis.delacruz@gmail.com>
2017-06-20 12:27:11 -0500

    #87 Adding locale resolver test

---
+++ src/test/groovy/com/jos/dem/vetlog/unit/LocaleResolverSpec.groovy
@@ -0,0 +1,39 @@
+/*
+Copyright 2017 José Luis De la Cruz Morales joseluis.delacruz@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.jos.dem.vetlog.unit
+
+import javax.servlet.http.HttpServletRequest
+
+import com.jos.dem.vetlog.helper.LocaleResolver
+
+import spock.lang.Specification
+
+class LocaleResolverSpec extends Specification {
+
+	LocaleResolver resolver = new LocaleResolver()
+
+	void "should get locale default"(){
+		given:'A request'
+		  HttpServletRequest request = Mock(HttpServletRequest)
+    when:'We call resolve'
+      Locale result = resolver.resolveLocale(request)
+    then:'We expect default'
+      Locale.getDefault() == result  
+
+	}
+	
+}
