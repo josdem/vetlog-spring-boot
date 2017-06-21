8b2f16c92b2539467bd05417ab6168df2718def1
josdem <joseluis.delacruz@gmail.com>
2017-06-20 20:23:11 -0500

    #87 Fixing test

--- src/test/groovy/com/jos/dem/vetlog/unit/ChangePasswordValidatorSpec.groovy
+++ src/test/groovy/com/jos/dem/vetlog/unit/ChangePasswordValidatorSpec.groovy
@@ -21,7 +21,6 @@ import org.springframework.validation.Errors
 import com.jos.dem.vetlog.command.Command
 import com.jos.dem.vetlog.command.ChangePasswordCommand
 import com.jos.dem.vetlog.validator.ChangePasswordValidator
-import com.jos.dem.vetlog.service.LocaleService
 
 import spock.lang.Specification
 
@@ -30,20 +29,14 @@ class ChangePasswordValidatorSpec extends Specification {
   ChangePasswordValidator validator = new ChangePasswordValidator()
 
   Errors errors = Mock(Errors)
-  LocaleService localeService = Mock(LocaleService)
-
-  def setup(){
-    validator.localeService = localeService
-  }
 
   void "should not validate since passwords are not equals"(){
     given:"A user command"
       Command command = new ChangePasswordCommand(token:'token',password:'password', passwordConfirmation:'p4ssword')
     when:"We validate passwords"
-      localeService.getMessage('user.validation.password.equals') >> 'The passwords are not equals'
       validator.validate(command, errors)
     then:"We expect valiation failed"
-    1 * errors.rejectValue('password', 'error.password', 'The passwords are not equals')
+    1 * errors.rejectValue('password', 'user.error.password.equals')
   }
 
 }
--- src/test/groovy/com/jos/dem/vetlog/unit/UserValidatorSpec.groovy
+++ src/test/groovy/com/jos/dem/vetlog/unit/UserValidatorSpec.groovy
@@ -23,7 +23,6 @@ import spock.lang.Specification
 import  com.jos.dem.vetlog.model.User
 import com.jos.dem.vetlog.command.Command
 import com.jos.dem.vetlog.command.UserCommand
-import com.jos.dem.vetlog.service.LocaleService
 import com.jos.dem.vetlog.service.UserService
 import com.jos.dem.vetlog.validator.UserValidator
 
@@ -31,11 +30,9 @@ class UserValidatorSpec extends Specification {
 
   UserValidator validator = new UserValidator()
   Errors errors = Mock(Errors)
-  LocaleService localeService = Mock(LocaleService)
   UserService userService = Mock(UserService)
 
   def setup(){
-    validator.localeService = localeService
     validator.userService = userService
   }
 
@@ -43,60 +40,54 @@ class UserValidatorSpec extends Specification {
     given:"A user command"
       Command command = new UserCommand(username:'josdem',password:'password', passwordConfirmation:'p4ssword', firstname:'josdem',lastname:'lastname',email:'josdem@email.com')
     when:"We validate passwords"
-      localeService.getMessage('user.validation.password.equals') >> 'The passwords are not equals'
       validator.validate(command, errors)
     then:"We expect valiation failed"
-    1 * errors.rejectValue('password', 'error.password', 'The passwords are not equals')
+    1 * errors.rejectValue('password', 'user.error.password.equals')
   }
 
   void "should vaidate an user command"(){
     given:"A user command"
       Command command = new UserCommand(username:'josdem',password:'password', passwordConfirmation:'password', firstname:'josdem',lastname:'lastname',email:'josdem@email.com')
     when:"We validate passwords"
-      localeService.getMessage(_ as String) >> 'Passwords are bad formed'
       validator.validate(command, errors)
     then:"We expect everything is going to be all right"
-    0 * errors.rejectValue('password', 'error.password', _ as String)
+    0 * errors.rejectValue('password', _ as String)
   }
 
   void "should accept characters and numbers in password"(){
     given:"A user command"
       Command command = new UserCommand(username:'josdem',password:'p4ssword', passwordConfirmation:'p4ssword', firstname:'josdem',lastname:'lastname',email:'josdem@email.com')
     when:"We validate passwords"
-      localeService.getMessage(_ as String) >> 'Passwords are bad formed'
       validator.validate(command, errors)
     then:"We expect everything is going to be all right"
-    0 * errors.rejectValue('password', 'error.password', _ as String)
+    0 * errors.rejectValue('password', _ as String)
   }
 
   void "should accept dash character in password"(){
     given:"A user command"
       Command command = new UserCommand(username:'josdem',password:'pa-4ssword', passwordConfirmation:'pa-4ssword', firstname:'josdem',lastname:'lastname',email:'josdem@email.com')
     when:"We validate passwords"
-      localeService.getMessage(_ as String) >> 'Passwords are bad formed'
       validator.validate(command, errors)
     then:"We expect everything is going to be all right"
-    0 * errors.rejectValue('password', 'error.password', _ as String)
+    0 * errors.rejectValue('password', _ as String)
   }
 
   void "should accept underscore character in password"(){
     given:"A user command"
       Command command = new UserCommand(username:'josdem',password:'pa_4ssword', passwordConfirmation:'pa_4ssword', firstname:'josdem',lastname:'lastname',email:'josdem@email.com')
     when:"We validate passwords"
-      localeService.getMessage(_ as String) >> 'Passwords are bad formed'
       validator.validate(command, errors)
     then:"We expect everything is going to be all right"
-    0 * errors.rejectValue('password', 'error.password', _ as String)
+    0 * errors.rejectValue('password', _ as String)
   }
 
   void "should accept dot character in password"(){
     given:"A user command"
       Command command = new UserCommand(username:'josdem',password:'pa.4ssword', passwordConfirmation:'pa.4ssword', firstname:'josdem',lastname:'lastname',email:'josdem@email.com')
     when:"We validate passwords"
-      localeService.getMessage(_ as String) >> 'Passwords are bad formed'
       validator.validate(command, errors)
     then:"We expect everything is going to be all right"
-    0 * errors.rejectValue('password', 'error.password', _ as String)
+    0 * errors.rejectValue('password', _ as String)
   }
 
   void "should not duplicate an username"(){
@@ -105,11 +96,10 @@ class UserValidatorSpec extends Specification {
     and:"User"
       User user = new User()
     when:"We validate user and user already exist by username"
-      localeService.getMessage('user.validation.duplicated.username') >> 'This user is already taken'
       userService.getByUsername('josdem') >> user
       validator.validate(command, errors)
     then:"We expected an error"
-    1 * errors.rejectValue('username', 'error.username', 'This user is already taken')
+    1 * errors.rejectValue('username', 'user.error.duplicated.username')
   }
 
   void "should not duplicate an email"(){
@@ -118,11 +108,10 @@ class UserValidatorSpec extends Specification {
     and:"User"
       User user = new User()
     when:"We validate user and user already exist by email"
-      localeService.getMessage('user.validation.duplicated.email') >> 'This email is already taken'
       userService.getByEmail('josdem@email.com') >> user
       validator.validate(command, errors)
     then:"We expected an error"
-    1 * errors.rejectValue('email', 'error.email', 'This email is already taken')
+    1 * errors.rejectValue('email', 'user.error.duplicated.email')
   }
 
 }
