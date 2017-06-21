28c8d96df230656aaf4a620500ef0241991e59b1
josdem <joseluis.delacruz@gmail.com>
2017-06-20 20:58:12 -0500

    #87 Fixing test

--- src/test/groovy/com/jos/dem/vetlog/unit/LocaleResolverSpec.groovy
+++ src/test/groovy/com/jos/dem/vetlog/unit/LocaleResolverSpec.groovy
@@ -32,8 +32,29 @@ class LocaleResolverSpec extends Specification {
     when:'We call resolve'
       Locale result = resolver.resolveLocale(request)
     then:'We expect default'
-      Locale.getDefault() == result  
-
+      Locale.getDefault() == result
 	}
-	
+
+  void "should get en-US as locale"(){
+		given:'A request'
+		  HttpServletRequest request = Mock(HttpServletRequest)
+    when:'We call resolve'
+      request.getHeader('Accept-Language') >> 'en-US,en;q=0.8'
+      Locale result = resolver.resolveLocale(request)
+    then:'We expect default'
+      result  == new Locale('en')
+	}
+
+  void "should get es-MX as locale"(){
+		given:'A request'
+		  HttpServletRequest request = Mock(HttpServletRequest)
+    when:'We call resolve'
+      request.getHeader('Accept-Language') >> 'es-MX,en-US;q=0.7,en;q=0.3'
+      Locale result = resolver.resolveLocale(request)
+    then:'We expect default'
+      result  == new Locale('es')
+	}
+
+
+
 }
