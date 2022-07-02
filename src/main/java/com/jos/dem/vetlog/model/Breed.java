a36f17a1d0dd5b24bb52934922235d465c234ae1
josdem <joseluis.delacruz@gmail.com>
2022-07-02 13:36:37 -0400

    #108 Moving model from Groovy to Java

---
+++ src/main/java/com/jos/dem/vetlog/model/Breed.java
@@ -0,0 +1,45 @@
+/*
+Copyright 2022 José Luis De la Cruz Morales joseluis.delacruz@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.jos.dem.vetlog.model;
+
+import com.jos.dem.vetlog.enums.PetType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+public class Breed {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    @Enumerated(STRING)
+    private PetType type;
+    @Column(nullable = false)
+    private Date dateCreated = new Date();
+
+}
