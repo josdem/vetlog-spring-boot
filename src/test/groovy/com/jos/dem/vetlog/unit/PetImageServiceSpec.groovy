bc388ad04dac6f6747e89d1b11b0b7589530b0c2
josdem <joseluis.delacruz@gmail.com>
2022-07-07 16:57:25 -0400

    #109 Fixing pet image service spec

--- src/test/groovy/com/jos/dem/vetlog/unit/PetImageServiceSpec.groovy
+++ src/test/groovy/com/jos/dem/vetlog/unit/PetImageServiceSpec.groovy
@@ -1,28 +1,26 @@
 package com.jos.dem.vetlog.unit
 
-import com.jos.dem.vetlog.model.Pet
 import com.jos.dem.vetlog.model.PetImage
+import com.jos.dem.vetlog.repository.PetImageRepository
 import com.jos.dem.vetlog.service.PetImageService
 import com.jos.dem.vetlog.service.impl.PetImageServiceImpl
-import com.jos.dem.vetlog.repository.PetImageRepository
-
 import spock.lang.Specification
 
 class PetImageServiceSpec extends Specification {
 
-  PetImageService service = new PetImageServiceImpl()
+    PetImageService service = new PetImageServiceImpl()
 
-  PetImageRepository petImageRepository = Mock(PetImageRepository)
+    PetImageRepository petImageRepository = Mock(PetImageRepository)
 
-  def setup(){
-    service.petImageRepository = petImageRepository
-  }
+    def setup() {
+        service.petImageRepository = petImageRepository
+    }
 
-  void "should save a pet image"(){
-    when:"I save a pet image"
-      PetImage result = service.save()
-    then:"I expect an image pet saved"
-      result.uuid.length() == 32
-      1 * petImageRepository.save(_ as PetImage)
-  }
+    void "should save a pet image"() {
+        when: "I save a pet image"
+        PetImage result = service.save()
+        then: "I expect an image pet saved"
+        result.uuid.length() == 36
+        1 * petImageRepository.save(_ as PetImage)
+    }
 }
