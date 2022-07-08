95b56cec21646794964b69ecb44d3d4e8f445d5b
josdem <joseluis.delacruz@gmail.com>
2022-07-08 12:00:19 -0400

    #110 Rewritting tables

--- src/main/resources/db/migration/V1__vetlog_master.sql
+++ src/main/resources/db/migration/V1__vetlog_master.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `registration_code`
+--
+
+DROP TABLE IF EXISTS `registration_code`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registration_code` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `registration_code`
+--
+
+LOCK TABLES `registration_code` WRITE;
+/*!40000 ALTER TABLE `registration_code` DISABLE KEYS */;
+/*!40000 ALTER TABLE `registration_code` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `breed`
 --
 
@@ -42,6 +68,36 @@ INSERT INTO `breed` VALUES (1,'2017-06-03 21:23:01','Labrador','DOG'),(2,'2017-0
 UNLOCK TABLES;
 
 --
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `account_non_expired` bit(1) NOT NULL,
+  `account_non_locked` bit(1) NOT NULL,
+  `credentials_non_expired` bit(1) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `enabled` bit(1) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+--
 -- Table structure for table `pet`
 --
 
@@ -64,10 +120,8 @@ CREATE TABLE `pet` (
   `user_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FKj3j9664ee510hnt2pkd7on7dl` (`adopter_id`),
-  KEY `FK87x36k4uwowulincp57w6vv7` (`pet_adoption_id`),
   KEY `FKiqbjbaml7gtwulqptktmsi5dc` (`breed_id`),
   KEY `FK9hxka0oqkd15dmqstdarori08` (`user_id`),
-  CONSTRAINT `FK87x36k4uwowulincp57w6vv7` FOREIGN KEY (`pet_adoption_id`) REFERENCES `pet_adoption` (`id`),
   CONSTRAINT `FK9hxka0oqkd15dmqstdarori08` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `FKiqbjbaml7gtwulqptktmsi5dc` FOREIGN KEY (`breed_id`) REFERENCES `breed` (`id`),
   CONSTRAINT `FKj3j9664ee510hnt2pkd7on7dl` FOREIGN KEY (`adopter_id`) REFERENCES `user` (`id`)
@@ -77,6 +131,7 @@ CREATE TABLE `pet` (
 --
 -- Dumping data for table `pet`
 --
+
 
 --
 -- Table structure for table `pet_adoption`
@@ -159,71 +214,3 @@ LOCK TABLES `pet_log` WRITE;
 /*!40000 ALTER TABLE `pet_log` DISABLE KEYS */;
 /*!40000 ALTER TABLE `pet_log` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `registration_code`
---
-
-DROP TABLE IF EXISTS `registration_code`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `registration_code` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `date_created` datetime NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `status` int(11) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `registration_code`
---
-
-LOCK TABLES `registration_code` WRITE;
-/*!40000 ALTER TABLE `registration_code` DISABLE KEYS */;
-/*!40000 ALTER TABLE `registration_code` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `account_non_expired` bit(1) NOT NULL,
-  `account_non_locked` bit(1) NOT NULL,
-  `credentials_non_expired` bit(1) NOT NULL,
-  `date_created` datetime NOT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `enabled` bit(1) NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  `mobile` varchar(255) DEFAULT NULL,
-  `password` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-06-04 10:51:42
