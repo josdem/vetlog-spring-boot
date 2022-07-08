b5314029d8c665258755b8bbdb38c8c86c169eda
josdem <joseluis.delacruz@gmail.com>
2022-07-08 10:33:42 -0400

    Adding mising component annotation

--- src/main/java/com/jos/dem/vetlog/binder/PetLogBinder.java
+++ src/main/java/com/jos/dem/vetlog/binder/PetLogBinder.java
@@ -1,10 +1,27 @@
+/*
+Copyright 2022 Jose Morales joseluis.delacruz@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.jos.dem.vetlog.binder;
 
 import com.jos.dem.vetlog.command.Command;
-import com.jos.dem.vetlog.command.PetCommand;
 import com.jos.dem.vetlog.command.PetLogCommand;
 import com.jos.dem.vetlog.model.PetLog;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PetLogBinder {
 
     public PetLog bind(Command command) {
