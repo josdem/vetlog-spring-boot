1c52fa9477d999b78694c493b9c1d30ac342a09f
josdem <joseluis.delacruz@gmail.com>
2022-07-16 15:18:21 -0400

    #118 Moving Vetlog and User exceptions to Java

--- src/main/java/com/jos/dem/vetlog/exception/VetlogException.java
+++ src/main/java/com/jos/dem/vetlog/exception/VetlogException.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 José Luis De la Cruz Morales joseluis.delacruz@gmail.com
+Copyright 2022 Jose Morales joseluis.delacruz@gmail.com
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,14 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
  */
 
-package com.jos.dem.vetlog.exception
+package com.jos.dem.vetlog.exception;
 
-import java.lang.RuntimeException
+public class VetlogException extends RuntimeException {
 
-class UserNotFoundException extends RuntimeException {
-
-  UserNotFoundException(String message){
-    super(message)
-  }
+    public VetlogException(String message) {
+        super(message);
+    }
 
 }
