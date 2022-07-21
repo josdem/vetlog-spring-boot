669142fb7836fcc2b779524a34b1a1937614da75
josdem <joseluis.delacruz@gmail.com>
2022-07-21 17:02:17 -0400

    #121 Updating script

--- src/main/resources/static/javascript/selector.js
+++ src/main/resources/static/javascript/selector.js
@@ -1,20 +1,20 @@
-$("#typeSelector").change(function () {
-  var str = $("#typeSelector option:selected").text()
-  console.log(str)
+const updateBreedType = () => {
+  let str = $("#typeSelector option:selected").text()
+  console.log("typeSelector: " + str)
   $.ajax($("#breedsByTypeUrl").val(), {
     method: 'GET',
     dataType: 'json',
     data: {
-      type: str
+        type: str
     }
-  }).done(function(data) {
-    $("#breedSelector").empty()
-    data.forEach(function(it){
-      $("#breedSelector").append($("<option></option>").attr("value", it.id).text(it.name))
+    }).done(function(data) {
+      $("#breedSelector").empty()
+      data.forEach(function(it){
+        $("#breedSelector").append($("<option></option>").attr("value", it.id).text(it.name))
+      })
+      $("#breedSelector").val($("#breed").val())
+    }).fail(function(data, status){
+      console.log(data, status)
     })
-    $("#breedSelector").val($("#breed").val())
-  }).fail(function(data, status){
-    console.log(data, status)
-  });
-}).change()
+}
 
