46360f8fb4622c70566c4a5e7114cf8e6a3a6e7b
josdem <joseluis.delacruz@gmail.com>
2022-09-07 15:34:49 -0500

    #150 Moving AfterThrowingAdvice from Groovy to Java

--- src/main/java/com/jos/dem/vetlog/advice/AfterThrowingAdvice.java
+++ src/main/java/com/jos/dem/vetlog/advice/AfterThrowingAdvice.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 José Luis De la Cruz Morales joseluis.delacruz@gmail.com
+Copyright 2022 Jose Morales joseluis.delacruz@gmail.com
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,26 +14,24 @@ See the License for the specific language governing permissions and
 limitations under the License.
  */
 
-package com.jos.dem.vetlog.advice
+package com.jos.dem.vetlog.advice;
 
-import org.aspectj.lang.annotation.AfterThrowing
-import org.aspectj.lang.annotation.Aspect
-import org.springframework.stereotype.Component
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-
-import com.jos.dem.vetlog.exception.BusinessException
+import com.jos.dem.vetlog.exception.BusinessException;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-class AfterThrowingAdvice {
+public class AfterThrowingAdvice {
 
-  Logger log = LoggerFactory.getLogger(this.class)
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
   @AfterThrowing(pointcut = "execution(* com.jos.dem.vetlog.service..**.*(..))", throwing = "ex")
   public void doRecoveryActions(RuntimeException ex) {
-    log.info "Wrapping exception ${ex}"
-    throw new BusinessException(ex.getMessage(), ex)
+    log.info("Wrapping exception: " + ex.getMessage());
+    throw new BusinessException(ex.getMessage(), ex);
   }
-
 }
