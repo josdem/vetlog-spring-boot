de5fdc51720589c6de17ee3836f6fe374606bdef
josdem <joseluis.delacruz@gmail.com>
2022-09-11 16:15:27 -0500

    #151 Adding invalid token test

--- src/test/java/com/jos/dem/vetlog/model/RegistrationCodeTest.java
+++ src/test/java/com/jos/dem/vetlog/model/RegistrationCodeTest.java
@@ -1,5 +1,6 @@
 package com.jos.dem.vetlog.model;
 
+import com.jos.dem.vetlog.enums.RegistrationCodeStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
@@ -23,5 +25,13 @@ class RegistrationCodeTest {
     assertEquals(7, DAYS.between(registrationCode.getDateCreated(), LocalDate.now().plusDays(7)));
     assertEquals(36, registrationCode.getToken().length());
     assertTrue(registrationCode.isValid());
+  }
+
+  @Test
+  @DisplayName("invalidating a token")
+  void shouldInvalidateToken(TestInfo testInfo) {
+    log.info("Running: {}", testInfo.getDisplayName());
+    registrationCode.setStatus(RegistrationCodeStatus.INVALID);
+    assertFalse(registrationCode.isValid());
   }
 }
