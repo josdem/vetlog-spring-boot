b9b90328899d5ce6bf1b1b175b577cf097d71bbd
josdem <joseluis.delacruz@gmail.com>
2022-09-11 15:36:16 -0500

    #151 Refactor

--- src/test/java/com/jos/dem/vetlog/helper/LocaleResolverTest.java
+++ src/test/java/com/jos/dem/vetlog/helper/LocaleResolverTest.java
@@ -16,12 +16,12 @@ import static org.mockito.Mockito.when;
 class LocaleResolverTest {
 
   private LocaleResolver localeResolver = new LocaleResolver();
+  private HttpServletRequest request = mock(HttpServletRequest.class);
 
   @Test
   @DisplayName("getting default locale")
   void shouldGetDefaultLocale(TestInfo testInfo) {
     log.info("Running: {}", testInfo.getDisplayName());
-    HttpServletRequest request = mock(HttpServletRequest.class);
     Locale result = localeResolver.resolveLocale(request);
     assertEquals(new Locale("en"), result);
   }
@@ -30,7 +30,6 @@ class LocaleResolverTest {
   @DisplayName("getting english from headers")
   void shouldGetEnglishFromHeaders(TestInfo testInfo){
     log.info("Running: {}", testInfo.getDisplayName());
-    HttpServletRequest request = mock(HttpServletRequest.class);
     when(request.getHeader("Accept-Language")).thenReturn("en-US,en;q=0.8");
     Locale result = localeResolver.resolveLocale(request);
     assertEquals(new Locale("en"), result);
@@ -40,7 +39,6 @@ class LocaleResolverTest {
   @DisplayName("getting spanish from headers")
   void shouldGetSpanishFromHeaders(TestInfo testInfo){
     log.info("Running: {}", testInfo.getDisplayName());
-    HttpServletRequest request = mock(HttpServletRequest.class);
     when(request.getHeader("Accept-Language")).thenReturn("es-MX,en-US;q=0.7,en;q=0.3");
     Locale result = localeResolver.resolveLocale(request);
     assertEquals(new Locale("es"), result);
