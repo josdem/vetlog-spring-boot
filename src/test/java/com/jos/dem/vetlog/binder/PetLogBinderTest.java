91d19ca7f9afd0f8d5166da1176e040f6da5dced
josdem <joseluis.delacruz@gmail.com>
2022-09-13 15:05:57 -0500

    #153 Refactor

--- src/test/java/com/jos/dem/vetlog/binder/PetLogBinderTest.java
+++ src/test/java/com/jos/dem/vetlog/binder/PetLogBinderTest.java
@@ -3,6 +3,7 @@ package com.jos.dem.vetlog.binder;
 import com.jos.dem.vetlog.command.PetLogCommand;
 import com.jos.dem.vetlog.model.PetLog;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
@@ -18,15 +19,23 @@ class PetLogBinderTest {
   @DisplayName("binding a pet log command")
   void shouldBindPetLogCommand(TestInfo testInfo) {
     log.info("Running: {}", testInfo.getDisplayName());
+    PetLogCommand petLogCommand = getPetLogCommand();
+
+    PetLog result = binder.bind(petLogCommand);
+
+    assertEquals("Margarita Morales", result.getVetName());
+    assertEquals("Digestion issues", result.getSigns());
+    assertEquals("Constipation", result.getDiagnosis());
+    assertEquals("Stool Ease", result.getMedicine());
+  }
+
+  @NotNull
+  private PetLogCommand getPetLogCommand() {
     PetLogCommand petLogCommand = new PetLogCommand();
     petLogCommand.setVetName("Margarita Morales");
     petLogCommand.setSigns("Digestion issues");
     petLogCommand.setDiagnosis("Constipation");
     petLogCommand.setMedicine("Stool Ease");
-    PetLog result = binder.bind(petLogCommand);
-    assertEquals("Margarita Morales", result.getVetName());
-    assertEquals("Digestion issues", result.getSigns());
-    assertEquals("Constipation", result.getDiagnosis());
-    assertEquals("Stool Ease", result.getMedicine());
+    return petLogCommand;
   }
 }
