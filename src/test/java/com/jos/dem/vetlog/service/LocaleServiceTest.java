d16e868c055de442f1482d3497d04165d1987997
josdem <joseluis.delacruz@gmail.com>
2022-09-14 14:59:55 -0400

    #154 Adding breed service test

---
+++ src/test/java/com/jos/dem/vetlog/service/LocaleServiceTest.java
@@ -0,0 +1,37 @@
+package com.jos.dem.vetlog.service;
+
+import com.jos.dem.vetlog.helper.LocaleResolver;
+import com.jos.dem.vetlog.service.impl.LocaleServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.MessageSource;
+
+import static org.mockito.Mockito.when;
+
+@Slf4j
+class LocaleServiceTest {
+
+    private LocaleService service;
+
+    @Mock private MessageSource messageSource;
+    @Mock private LocaleResolver localeResolver;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+        service = new LocaleServiceImpl(messageSource, localeResolver);
+    }
+
+    @Test
+    @DisplayName("getting message by request")
+    void shouldGetMessageByRequest(TestInfo testInfo){
+        log.info("Running: {}", testInfo.getDisplayName());
+        String code = "code";
+        when(messageSource.getMessage(code, null, localeResolver.resolveLocale(request));)
+    }
+}
