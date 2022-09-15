c84971a508291043a4b2dc857286efa98511a7cf
josdem <joseluis.delacruz@gmail.com>
2022-09-15 09:15:23 -0400

    #154 Adding recovery service test

---
+++ src/test/java/com/jos/dem/vetlog/service/RecoverServiceTest.java
@@ -0,0 +1,55 @@
+package com.jos.dem.vetlog.service;
+
+import com.jos.dem.vetlog.command.Command;
+import com.jos.dem.vetlog.command.MessageCommand;
+import com.jos.dem.vetlog.repository.RegistrationCodeRepository;
+import com.jos.dem.vetlog.repository.UserRepository;
+import com.jos.dem.vetlog.service.impl.RecoveryServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.TestPropertySource;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@Slf4j
+@TestPropertySource(properties = {"template.register.name=registerTemplate"})
+class RecoverServiceTest {
+
+    private RecoveryService service;
+
+    @Mock
+    private RestService restService;
+    @Mock
+    private RegistrationService registrationService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private RegistrationCodeRepository repository;
+    @Mock
+    private LocaleService localeService;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        service = new RecoveryServiceImpl(restService, registrationService, userRepository, repository, localeService);
+    }
+
+    @Test
+    @DisplayName("send confirmation token")
+    void shouldSendConfirmationToken(TestInfo testInfo) throws IOException {
+        log.info("Running: {}", testInfo.getDisplayName());
+
+        service.sendConfirmationAccountToken("contact@josdem.io");
+
+        verify(registrationService).generateToken(anyString());
+        verify(restService).sendMessage(isA(MessageCommand.class));
+    }
+}
