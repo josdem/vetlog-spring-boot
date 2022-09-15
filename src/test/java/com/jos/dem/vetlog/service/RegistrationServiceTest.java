8eaed96cb653a5afca7fd5ce97d4b62f5592f5e3
josdem <joseluis.delacruz@gmail.com>
2022-09-15 13:07:41 -0400

    #154 Adding registration service test

---
+++ src/test/java/com/jos/dem/vetlog/service/RegistrationServiceTest.java
@@ -0,0 +1,41 @@
+package com.jos.dem.vetlog.service;
+
+import com.jos.dem.vetlog.model.RegistrationCode;
+import com.jos.dem.vetlog.repository.RegistrationCodeRepository;
+import com.jos.dem.vetlog.service.impl.RegistrationServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@Slf4j
+class RegistrationServiceTest {
+
+    private RegistrationService service;
+
+    @Mock private LocaleService localeService;
+    @Mock private RegistrationCodeRepository repository;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+        service = new RegistrationServiceImpl(localeService, repository);
+    }
+
+    @Test
+    @DisplayName("getting user by token")
+    void shouldGetUserByToken(TestInfo testInfo){
+        log.info("Running: {}", testInfo.getDisplayName());
+        String token = "token";
+        RegistrationCode registrationCode = new RegistrationCode();
+        registrationCode.setEmail("contact@josdem.io");
+        when(repository.findByToken(token)).thenReturn(registrationCode);
+        assertEquals("contact@josdem.io", service.findEmailByToken(token));
+    }
+}
