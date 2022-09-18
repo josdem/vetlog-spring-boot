46c9fa7058b6e524d0656a3c84cb3f2a3aff5aaf
josdem <joseluis.delacruz@gmail.com>
2022-09-18 13:41:47 -0500

    #157 Adding recovery controller test

---
+++ src/test/java/com/jos/dem/vetlog/controller/RecoveryControllerTest.java
@@ -0,0 +1,35 @@
+package com.jos.dem.vetlog.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+class RecoveryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("validating token")
+    void shouldValidateToken(TestInfo testInfo) throws Exception {
+        log.info("Running: {}", testInfo.getDisplayName());
+        mockMvc
+                .perform(get("/recovery/activate/token"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error"));
+    }
+
+}
