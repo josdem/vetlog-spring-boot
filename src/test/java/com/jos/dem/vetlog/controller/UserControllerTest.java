33477fa9ce7c9ae0bc2efe3120781bfb5abc856a
josdem <joseluis.delacruz@gmail.com>
2022-09-18 16:41:26 -0500

    #158 Adding user controller test

---
+++ src/test/java/com/jos/dem/vetlog/controller/UserControllerTest.java
@@ -0,0 +1,35 @@
+package com.jos.dem.vetlog.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("showing create user form")
+    void shouldShowCreateUserForm(TestInfo testInfo) throws Exception {
+        log.info("Running: {}", testInfo.getDisplayName());
+        mockMvc
+                .perform(get("/user/create"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("userCommand"))
+                .andExpect(view().name("user/create"));
+    }
+}
