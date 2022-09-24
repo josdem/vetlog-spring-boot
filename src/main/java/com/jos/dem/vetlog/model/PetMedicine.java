90ce0a7859c860b03cb3461842d95abbf76951ba
josdem <joseluis.delacruz@gmail.com>
2022-09-24 10:30:41 -0400

    #161 Adding Pet medicine model

---
+++ src/main/java/com/jos/dem/vetlog/model/PetMedicine.java
@@ -0,0 +1,41 @@
+/*
+Copyright 2022 Jose Morales joseluis.delacruz@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.jos.dem.vetlog.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Getter
+@Setter
+public class PetMedicine {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String uuid;
+
+}
