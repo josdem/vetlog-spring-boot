d92e4f8ebef9dfed5b328fa4dd08d9a3d96f382a
josdem <joseluis.delacruz@gmail.com>
2022-10-08 15:16:30 -0400

    #159 Adding licenses to the controllers

--- src/test/java/com/jos/dem/vetlog/controller/RecoveryControllerTest.java
+++ src/test/java/com/jos/dem/vetlog/controller/RecoveryControllerTest.java
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 Jose Morales joseluis.delacruz@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
 package com.jos.dem.vetlog.controller;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,36 +35,37 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class RecoveryControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Test
-  @DisplayName("validating token")
-  void shouldValidateToken(TestInfo testInfo) throws Exception {
-    log.info("Running: {}", testInfo.getDisplayName());
-    mockMvc
-        .perform(get("/recovery/activate/token"))
-        .andExpect(status().isOk())
-        .andExpect(view().name("error"));
-  }
+    @Test
+    @DisplayName("validating token")
+    void shouldValidateToken(TestInfo testInfo) throws Exception {
+        log.info("Running: {}", testInfo.getDisplayName());
+        mockMvc
+                .perform(get("/recovery/activate/token"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error"));
+    }
 
-  @Test
-  @DisplayName("getting email to change password")
-  void shouldRequestEmailToChangePassword(TestInfo testInfo) throws Exception {
-    log.info("Running: {}", testInfo.getDisplayName());
-    mockMvc
-        .perform(get("/recovery/password"))
-        .andExpect(status().isOk())
-        .andExpect(view().name("recovery/recoveryPassword"));
-  }
+    @Test
+    @DisplayName("getting email to change password")
+    void shouldRequestEmailToChangePassword(TestInfo testInfo) throws Exception {
+        log.info("Running: {}", testInfo.getDisplayName());
+        mockMvc
+                .perform(get("/recovery/password"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recovery/recoveryPassword"));
+    }
 
-  @Test
-  @DisplayName("showing change password forms")
-  void shouldShowChangePasswordForms(TestInfo testInfo) throws Exception {
-    log.info("Running: {}", testInfo.getDisplayName());
-    mockMvc
-        .perform(get("/recovery/forgot/token"))
-        .andExpect(status().isOk())
-        .andExpect(model().attributeExists("message"))
-        .andExpect(view().name("recovery/changePassword"));
-  }
+    @Test
+    @DisplayName("showing change password forms")
+    void shouldShowChangePasswordForms(TestInfo testInfo) throws Exception {
+        log.info("Running: {}", testInfo.getDisplayName());
+        mockMvc
+                .perform(get("/recovery/forgot/token"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("message"))
+                .andExpect(view().name("recovery/changePassword"));
+    }
 }
