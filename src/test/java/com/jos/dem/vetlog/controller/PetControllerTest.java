27a86be73f67552ce4e07bde50479db934861e7d
josdem <joseluis.delacruz@gmail.com>
2022-12-17 12:38:41 -0500

    #132 Adding pet edit test

--- src/test/java/com/jos/dem/vetlog/controller/PetControllerTest.java
+++ src/test/java/com/jos/dem/vetlog/controller/PetControllerTest.java
@@ -36,21 +36,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class PetControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("showing create pet form")
-    @WithMockUser(username = "josdem", password = "12345678", roles = "USER")
-    void shouldShowCreatePetForm(TestInfo testInfo) throws Exception {
-        log.info("Running: {}", testInfo.getDisplayName());
-        mockMvc
-                .perform(get("/pet/create"))
-                .andExpect(status().isOk())
-                .andExpect(model().attributeExists("petCommand"))
-                .andExpect(model().attributeExists("breeds"))
-                .andExpect(model().attributeExists("breedsByTypeUrl"))
-                .andExpect(view().name("pet/create"));
-    }
+  @Test
+  @DisplayName("showing create pet form")
+  @WithMockUser(username = "josdem", password = "12345678", roles = "USER")
+  void shouldShowCreatePetForm(TestInfo testInfo) throws Exception {
+    log.info("Running: {}", testInfo.getDisplayName());
+    mockMvc
+        .perform(get("/pet/create"))
+        .andExpect(status().isOk())
+        .andExpect(model().attributeExists("petCommand"))
+        .andExpect(model().attributeExists("breeds"))
+        .andExpect(model().attributeExists("breedsByTypeUrl"))
+        .andExpect(view().name("pet/create"));
+  }
 
+  @Test
+  @DisplayName("showing edit pet form")
+  @WithMockUser(username = "josdem", password = "12345678", roles = "USER")
+  void shouldShowEditPetForm(TestInfo testInfo) throws Exception {
+    log.info("Running: {}", testInfo.getDisplayName());
+    mockMvc
+        .perform(get("/pet/edit").param("uuid", "43443f72-68d5-473a-9158-729f0b9f70bb"))
+        .andExpect(status().isOk())
+        .andExpect(model().attributeExists("petCommand"))
+        .andExpect(model().attributeExists("breeds"))
+        .andExpect(model().attributeExists("breedsByTypeUrl"))
+        .andExpect(view().name("pet/edit"));
+  }
 }
