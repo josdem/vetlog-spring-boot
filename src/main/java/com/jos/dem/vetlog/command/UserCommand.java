44a4ab639807f45d470670dfb9496464524fcd64
josdem <joseluis.delacruz@gmail.com>
2024-08-05 13:22:25 -0400

    #266 Updating license information

--- src/main/java/com/jos/dem/vetlog/command/UserCommand.java
+++ src/main/java/com/jos/dem/vetlog/command/UserCommand.java
@@ -1,5 +1,6 @@
 /*
-Copyright 2023 Jose Morales joseluis.delacruz@gmail.com
+Copyright 2024 Jose Morales contact@josdem.io
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
