bee001863362fb14eb20bb3984813c3d3271554c
josdem <joseluis.delacruz@gmail.com>
2024-08-23 08:05:48 -0400

    #284 Using the same uuid for creating and edit

--- src/test/java/com/josdem/vetlog/controller/PetControllerTest.java
+++ src/test/java/com/josdem/vetlog/controller/PetControllerTest.java
@@ -19,6 +19,7 @@ package com.josdem.vetlog.controller;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -43,6 +44,8 @@ import org.springframework.web.context.WebApplicationContext;
 @SpringBootTest
 @AutoConfigureMockMvc
 class PetControllerTest {
+
+    public static final String PET_UUID = "5acd03eb-2795-4e92-85d0-92e142c44170";
 
     @Autowired
     private MockMvc mockMvc;
@@ -69,7 +72,7 @@ class PetControllerTest {
                         .file(image)
                         .with(csrf())
                         .param("name", "Cremita")
-                        .param("uuid", "5acd03eb-2795-4e92-85d0-92e142c44170")
+                        .param("uuid", PET_UUID)
                         .param("birthDate", "2024-08-22T09:28:00")
                         .param("dewormed", "true")
                         .param("vaccinated", "true")
@@ -87,8 +90,11 @@ class PetControllerTest {
     @WithMockUser(username = "josdem", password = "12345678", roles = "USER")
     void shouldShowEditPetForm(TestInfo testInfo) throws Exception {
         log.info("Running: {}", testInfo.getDisplayName());
-        mockMvc.perform(get("/pet/edit").param("uuid", "43443f72-68d5-473a-9158-729f0b9f70bb"))
+        mockMvc.perform(get("/pet/edit").param("uuid", PET_UUID))
                 .andExpect(status().isOk())
-                .andExpect(view().name("error"));
+                .andExpect(model().attributeExists("petCommand"))
+                .andExpect(model().attributeExists("breeds"))
+                .andExpect(model().attributeExists("breedsByTypeUrl"))
+                .andExpect(view().name("pet/edit"));
     }
 }
