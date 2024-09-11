23ccc0aaca57e935633d9a4150134bf565d4c090
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2024-09-12 00:12:04 +0900

    nod use import delete

--- src/main/java/com/josdem/vetlog/service/impl/PetLogServiceImpl.java
+++ src/main/java/com/josdem/vetlog/service/impl/PetLogServiceImpl.java
@@ -29,7 +29,6 @@ import com.josdem.vetlog.service.PetPrescriptionService;
 import jakarta.transaction.Transactional;
 import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
