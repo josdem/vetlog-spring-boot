62fefeedefbf0c358832bad8834be2ef7f276933
josdem <joseluis.delacruz@gmail.com>
2024-09-24 06:09:35 -0400

    Moving some columns to the master SQL

--- src/main/resources/db/migration/V1.7__adding_pet_missing_columns.sql
+++ src/main/resources/db/migration/V1.7__adding_pet_missing_columns.sql
@@ -1,5 +1,3 @@
- ALTER TABLE pet ADD COLUMN pet_id bigint DEFAULT NULL;
  ALTER TABLE pet ADD CONSTRAINT pet_id_constraint FOREIGN KEY (`pet_id`) REFERENCES pet_adoption (`id`);
- ALTER TABLE pet_log ADD COLUMN uuid varchar(255) NOT NULL;
  ALTER TABLE registration_code MODIFY status tinyint NOT NULL;
  ALTER TABLE user ADD COLUMN country_code varchar(10) NULL;
