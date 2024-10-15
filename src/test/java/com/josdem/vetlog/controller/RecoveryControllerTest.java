1bedc2bf68974ad75f2220623e2c380cf919bfd1
josdem <joseluis.delacruz@gmail.com>
2024-10-15 09:09:13 -0600

    #380 Adding missing recovery controller tests

--- src/test/java/com/josdem/vetlog/controller/RecoveryControllerTest.java
+++ src/test/java/com/josdem/vetlog/controller/RecoveryControllerTest.java
@@ -16,7 +16,9 @@ limitations under the License.
 
 package com.josdem.vetlog.controller;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
@@ -62,5 +64,23 @@ class RecoveryControllerTest {
         mockMvc.perform(get("/recovery/forgot/token"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recovery/changePassword"));
+    }
+
+    @Test
+    @DisplayName("user token not found")
+    void shouldNotFindUserToken(TestInfo testInfo) throws Exception {
+        log.info("Running: {}", testInfo.getDisplayName());
+        mockMvc.perform(get("/recovery/activate/token"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error"));
+    }
+
+    @Test
+    @DisplayName("not generating token for changing password due to user not found")
+    void shouldNotGenerateTokenToChangePassword(TestInfo testInfo) throws Exception {
+        log.info("Running: {}", testInfo.getDisplayName());
+        mockMvc.perform(post("/recovery/password").with(csrf()).param("email", "contact@josdem.io"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("error"));
     }
 }
