18e9d74a05b83be97d3f2a9f9dd2116fc5098b9b
josdem <joseluis.delacruz@gmail.com>
2024-10-15 08:56:08 -0600

    #380 Completing test

--- src/test/java/com/josdem/vetlog/controller/VetControllerTest.java
+++ src/test/java/com/josdem/vetlog/controller/VetControllerTest.java
@@ -16,21 +16,32 @@ limitations under the License.
 
 package com.josdem.vetlog.controller;
 
+import static com.josdem.vetlog.controller.PetControllerTest.PET_UUID;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import com.josdem.vetlog.enums.PetStatus;
+import com.josdem.vetlog.enums.PetType;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @Slf4j
 @SpringBootTest
@@ -39,6 +50,19 @@ class VetControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private final MockMultipartFile image =
+            new MockMultipartFile("mockImage", "image.jpg", "image/jpeg", "image".getBytes());
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
     @DisplayName("showing create vet form")
@@ -55,9 +79,34 @@ class VetControllerTest {
     @Transactional
     @DisplayName("searching by user")
     @WithMockUser(username = "admin", password = "12345678", roles = "ADMIN")
-    void shouldSearchPetsByUser(TestInfo testInfo){
+    void shouldSearchPetsByUser(TestInfo testInfo) throws Exception {
         log.info("Running: {}", testInfo.getDisplayName());
-        
+
+        registerPet();
+
+        mockMvc.perform(post("/vet/search").with(csrf()).param("username", "josdem"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("pets"))
+                .andExpect(model().attributeExists("defaultImage"))
+                .andExpect(view().name("vet/list"));
     }
 
+    private void registerPet() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/pet/save")
+                        .file(image)
+                        .with(csrf())
+                        .param("name", "Cremita")
+                        .param("uuid", PET_UUID)
+                        .param("birthDate", "2024-08-22T09:28:00")
+                        .param("dewormed", "true")
+                        .param("vaccinated", "true")
+                        .param("sterilized", "true")
+                        .param("breed", "11")
+                        .param("user", "1")
+                        .param("status", PetStatus.OWNED.toString())
+                        .param("type", PetType.DOG.toString()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("pet/create"));
+    }
 }
