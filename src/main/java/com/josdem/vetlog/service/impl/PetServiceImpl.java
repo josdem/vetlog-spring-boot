d6cc1280d0f30d160d6541220173f9eaf15a9a05
josdem <joseluis.delacruz@gmail.com>
2024-10-29 06:16:36 -0600

    #392 Making delete pet transactional

--- src/main/java/com/josdem/vetlog/service/impl/PetServiceImpl.java
+++ src/main/java/com/josdem/vetlog/service/impl/PetServiceImpl.java
@@ -108,7 +108,7 @@ public class PetServiceImpl implements PetService {
         });
     }
 
-    @Override
+    @Transactional
     public void deletePetById(Long id) {
         var pet = petRepository.findById(id).orElseThrow(() -> new BusinessException(NO_PET_WAS_FOUND_WITH_ID + id));
         if (pet.getStatus() == PetStatus.IN_ADOPTION) {
