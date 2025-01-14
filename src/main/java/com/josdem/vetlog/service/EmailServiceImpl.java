2ec9ffc32d24bac40f9f51aa118f724dbc91ec4b
josdem <joseluis.delacruz@gmail.com>
2025-01-14 05:49:45 -0600

    #434 Adding email service and implementation

---
+++ src/main/java/com/josdem/vetlog/service/EmailServiceImpl.java
@@ -0,0 +1,55 @@
+/*
+  Copyright 2025 Jose Morales contact@josdem.io
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.josdem.vetlog.service;
+
+import com.josdem.vetlog.command.MessageCommand;
+import com.josdem.vetlog.config.TemplateProperties;
+import com.josdem.vetlog.exception.BusinessException;
+import com.josdem.vetlog.model.User;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@EnableConfigurationProperties(TemplateProperties.class)
+public class EmailServiceImpl implements EmailService {
+
+    @Value("${token}")
+    private String clientToken;
+
+    private final RestService restService;
+    private final TemplateProperties templateProperties;
+
+    public void sendWelcomeEmail(User user) {
+        log.info("Sending welcome email to: {}", user.getFirstName());
+        try {
+            var command = new MessageCommand();
+            command.setEmail(user.getEmail());
+            command.setName(user.getFirstName());
+            command.setTemplate(templateProperties.getWelcome());
+            command.setToken(clientToken);
+            restService.sendMessage(command);
+        } catch (IOException ioe) {
+            throw new BusinessException(ioe.getMessage());
+        }
+    }
+}
---
+++ src/test/java/com/josdem/vetlog/service/EmailServiceTest.java
@@ -0,0 +1,64 @@
+/*
+  Copyright 2025 Jose Morales contact@josdem.io
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+package com.josdem.vetlog.service;
+
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.josdem.vetlog.command.MessageCommand;
+import com.josdem.vetlog.config.TemplateProperties;
+import com.josdem.vetlog.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+@Slf4j
+class EmailServiceTest {
+
+    private EmailService emailService;
+
+    @Mock
+    private RestService restService;
+
+    @Mock
+    private TemplateProperties templateProperties;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+        emailService = new EmailServiceImpl(restService, templateProperties);
+    }
+
+    @Test
+    @DisplayName("sending a welcome email")
+    void shouldSendWelcomeEmail(TestInfo testInfo) throws Exception {
+        log.info(testInfo.getDisplayName());
+        when(templateProperties.getWelcome()).thenReturn("welcome.ftl");
+        var user = new User();
+        user.setFirstName("Jose");
+        user.setEmail("contact@josdem.io");
+
+        emailService.sendWelcomeEmail(user);
+
+        verify(restService).sendMessage(isA(MessageCommand.class));
+    }
+}
