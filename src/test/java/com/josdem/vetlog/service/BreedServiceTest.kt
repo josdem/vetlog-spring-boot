1467d6d455a4b984e134873bdebcf456dba6fdd6
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2025-02-04 23:20:47 +0900

    change kotlin code BreedServiceTest

---
+++ src/test/java/com/josdem/vetlog/service/BreedServiceTest.kt
@@ -0,0 +1,60 @@
+/*
+Copyright 2024 Jose Morales contact@josdem.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.josdem.vetlog.service
+
+import com.josdem.vetlog.model.Breed
+import com.josdem.vetlog.repository.BreedRepository
+import com.josdem.vetlog.service.impl.BreedServiceImpl
+import com.josdem.vetlog.enums.PetType
+import org.junit.jupiter.api.*
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.whenever
+import org.mockito.kotlin.verify
+import org.slf4j.LoggerFactory
+
+internal class BreedServiceTest {
+
+    private lateinit var service: BreedService
+
+    @Mock
+    private lateinit var breedRepository: BreedRepository
+
+    companion object {
+        private val log = LoggerFactory.getLogger(BreedServiceTest::class.java)
+    }
+
+    @BeforeEach
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        service = BreedServiceImpl(breedRepository)
+    }
+
+    @Test
+    @DisplayName("Getting breeds by type")
+    fun shouldGetBreedsByType(testInfo: TestInfo) {
+        log.info("Running: {}", testInfo.displayName)
+
+        val mockBreeds = listOf(Breed()) // Kotlin の listOf を使用
+        whenever(breedRepository.findByType(PetType.DOG)).thenReturn(mockBreeds)
+
+        val breeds = service.getBreedsByType(PetType.DOG)
+
+        Assertions.assertTrue(breeds.isNotEmpty(), "should have a breed")
+        verify(breedRepository).findByType(PetType.DOG)
+    }
+}
