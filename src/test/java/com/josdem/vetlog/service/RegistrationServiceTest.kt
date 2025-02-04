1d356c111e333149d223c1a42c2c458da0f06e55
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2025-02-04 23:27:05 +0900

    change kotlin code RegistrationServiceTest

---
+++ src/test/java/com/josdem/vetlog/service/RegistrationServiceTest.kt
@@ -0,0 +1,66 @@
+/*
+Copyright 2024 Jose Morales contact@josdem.io
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+package com.josdem.vetlog.service
+
+import com.josdem.vetlog.model.RegistrationCode
+import com.josdem.vetlog.repository.RegistrationCodeRepository
+import com.josdem.vetlog.service.impl.RegistrationServiceImpl
+import org.junit.jupiter.api.*
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.*
+import org.slf4j.LoggerFactory
+import java.util.*
+
+internal class RegistrationServiceTest {
+    private lateinit var service: RegistrationService
+
+    @Mock
+    private lateinit var localeService: LocaleService
+
+    @Mock
+    private lateinit var repository: RegistrationCodeRepository
+
+    companion object {
+        private val log = LoggerFactory.getLogger(RegistrationServiceTest::class.java)
+        private const val TOKEN = "token"
+        private const val EMAIL = "contact@josdem.io"
+    }
+
+    @BeforeEach
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        service = RegistrationServiceImpl(localeService, repository)
+    }
+
+    @Test
+    @DisplayName("Getting user by token")
+    fun shouldGetUserByToken(testInfo: TestInfo) {
+        log.info("Running: {}", testInfo.displayName)
+        val registrationCode = RegistrationCode().apply { email = EMAIL }
+        whenever(repository.findByToken(TOKEN)).thenReturn(Optional.of(registrationCode))
+        Assertions.assertEquals(EMAIL, service.findEmailByToken(TOKEN).get())
+    }
+
+    @Test
+    @DisplayName("Generating token")
+    fun shouldGenerateToken(testInfo: TestInfo) {
+        log.info("Running: {}", testInfo.displayName)
+        val token = service.generateToken(EMAIL)
+        Assertions.assertEquals(36, token.length)
+        verify(repository).save(any<RegistrationCode>())
+    }
+}
