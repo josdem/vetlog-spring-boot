9eb45dc19b47fb6ebcc36db5bef83b10bbc75726
T.H(kaminuma) <moshimo_0817@yahoo.co.jp>
2025-02-04 23:27:38 +0900

    change kotlin code RestServiceTest

---
+++ src/test/java/com/josdem/vetlog/service/RestServiceTest.kt
@@ -0,0 +1,48 @@
+package com.josdem.vetlog.service
+
+import com.josdem.vetlog.command.MessageCommand
+import com.josdem.vetlog.service.impl.RestServiceImpl
+import okhttp3.ResponseBody
+import org.junit.jupiter.api.*
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.*
+import org.slf4j.LoggerFactory
+import retrofit2.Call
+import retrofit2.Retrofit
+import java.io.IOException
+
+internal class RestServiceTest {
+    private lateinit var service: RestServiceImpl
+
+    @Mock
+    private lateinit var retrofit: Retrofit
+
+    companion object {
+        private val log = LoggerFactory.getLogger(RestServiceTest::class.java)
+    }
+
+    @BeforeEach
+    fun setup() {
+        MockitoAnnotations.openMocks(this)
+        service = RestServiceImpl(retrofit)
+    }
+
+    @Test
+    @DisplayName("Sending message")
+    @Throws(IOException::class)
+    fun shouldSendMessage(testInfo: TestInfo) {
+        log.info("Running: {}", testInfo.displayName)
+        val restService: RestService = mock()
+        val messageCommand: MessageCommand = mock()
+        val call: Call<ResponseBody> = mock()
+
+        whenever(retrofit.create(RestService::class.java)).thenReturn(restService)
+        whenever(restService.sendMessage(messageCommand)).thenReturn(call)
+
+        service.setup()
+        service.sendMessage(messageCommand)
+
+        verify(call).execute()
+    }
+}
