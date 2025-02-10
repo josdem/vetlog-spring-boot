81e3732686afec9a168bb5e562234e614ca72b51
josdem <joseluis.delacruz@gmail.com>
2025-02-10 09:37:35 -0600

    #467 Updating license year in Kotlin files

--- src/test/java/com/josdem/vetlog/service/EmailServiceTest.kt
+++ src/test/java/com/josdem/vetlog/service/EmailServiceTest.kt
@@ -13,6 +13,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
+
 package com.josdem.vetlog.service
 
 import com.josdem.vetlog.config.TemplateProperties
