b2746d1edab1a8dd0733ba8032494e463019283f
Harsh <rajh87897@gmail.com>
2025-02-19 00:02:31 +0530

    Updated V1.9__adding_breeds.sql - added sql queries

---
+++ src/main/resources/db/migration/V1.9__adding_breeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO `breed` (id, date_created, name, type) 
+VALUES 
+(80,'2025-02-18 15:05:01','Australian Cattle','DOG'),
+(81,'2025-02-18 15:05:01','Miniature Pinscher','DOG'),
+(82,'2025-02-18 15:05:01','Goldendoodle','DOG'),
+(83,'2025-02-18 15:05:01','Schnoodle','DOG'),
+(84,'2025-02-18 15:05:01','Akita','DOG'),
+(85,'2025-02-18 15:05:01','Alaskan Malamute','DOG'),
+(86,'2025-02-18 15:05:01','Bernese Mountain Dog','DOG'),
+(87,'2025-02-18 15:05:01','Basset Hound','DOG'),
+(88,'2025-02-18 15:05:01','Belgian Malinois','DOG'),
+(89,'2025-02-18 15:05:01','Chow Chow','DOG'),
+(90,'2025-02-18 15:05:01','Irish Setter','DOG'),
+(91,'2025-02-18 15:05:01','Rhodesian Ridgeback','DOG'),
+(92,'2025-02-18 15:05:01','Samoyed','DOG'),
+(93,'2025-02-18 15:05:01','Tibetan Mastiff','DOG');
