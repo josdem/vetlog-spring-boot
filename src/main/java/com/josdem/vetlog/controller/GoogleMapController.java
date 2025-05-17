c840b0901159284ae32e0907dcff491ac20cf6bb
josdem <joseluis.delacruz@gmail.com>
2025-05-17 11:36:40 -0600

    Reading Google maps key from configuration, and fixing test

--- src/main/java/com/josdem/vetlog/controller/GoogleMapController.java
+++ src/main/java/com/josdem/vetlog/controller/GoogleMapController.java
@@ -1,19 +1,20 @@
 package com.josdem.vetlog.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.josdem.vetlog.config.GoogleProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class GoogleMapController {
 
-    @Value("${google.maps.api.key}")
-    private String apiKey;
+    private final GoogleProperties googleProperties;
 
     @GetMapping("/map")
     public String showMap(Model model) {
-        model.addAttribute("apiKey", apiKey);
+        model.addAttribute("apiKey", googleProperties.getApiKey());
         return "map/map";
     }
 }
