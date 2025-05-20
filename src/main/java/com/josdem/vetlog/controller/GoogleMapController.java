d2d3cb27c93ec08ebe99642d0be398b240b8e3dc
josdem <joseluis.delacruz@gmail.com>
2025-05-20 16:48:43 -0600

    #579 Formatting code

--- src/main/java/com/josdem/vetlog/controller/GoogleMapController.java
+++ src/main/java/com/josdem/vetlog/controller/GoogleMapController.java
@@ -17,7 +17,6 @@
 package com.josdem.vetlog.controller;
 
 import com.josdem.vetlog.cache.ApplicationCache;
-import com.josdem.vetlog.command.PetGeolocation;
 import com.josdem.vetlog.config.GeolocationProperties;
 import com.josdem.vetlog.config.GoogleProperties;
 import com.josdem.vetlog.model.Location;
@@ -25,14 +24,13 @@ import com.josdem.vetlog.model.Pet;
 import com.josdem.vetlog.service.LocaleService;
 import com.josdem.vetlog.service.PetService;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Optional;
 
 @Slf4j
 @Controller
@@ -49,16 +47,14 @@ public class GoogleMapController {
         log.info("Pet id: {}", id);
         Location currentPetLocation = null;
         Pet pet = null;
-        if(id.isPresent()){
+        if (id.isPresent()) {
             log.info("Is present");
             pet = petService.getPetById(id.get());
             currentPetLocation = ApplicationCache.locations.get(pet.getId());
             log.info("Current pet location: {}", currentPetLocation);
         }
-        var latitude =
-                currentPetLocation != null ? currentPetLocation.getLat() : geolocationProperties.getLatitude();
-        var longitude =
-                currentPetLocation != null ? currentPetLocation.getLng() : geolocationProperties.getLongitude();
+        var latitude = currentPetLocation != null ? currentPetLocation.getLat() : geolocationProperties.getLatitude();
+        var longitude = currentPetLocation != null ? currentPetLocation.getLng() : geolocationProperties.getLongitude();
         var message = currentPetLocation != null
                 ? pet.getName() + localeService.getMessage("map.pet.geolocation", request)
                 : localeService.getMessage("map.pet.default", request);
