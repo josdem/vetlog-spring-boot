99818231e0f9f04d060914ef11337fce299f454a
josdem <joseluis.delacruz@gmail.com>
2025-05-20 16:44:24 -0600

    #579 Chola se fue a Japon y no me llevo

--- src/main/java/com/josdem/vetlog/controller/GoogleMapController.java
+++ src/main/java/com/josdem/vetlog/controller/GoogleMapController.java
@@ -17,31 +17,52 @@
 package com.josdem.vetlog.controller;
 
 import com.josdem.vetlog.cache.ApplicationCache;
+import com.josdem.vetlog.command.PetGeolocation;
 import com.josdem.vetlog.config.GeolocationProperties;
 import com.josdem.vetlog.config.GoogleProperties;
+import com.josdem.vetlog.model.Location;
+import com.josdem.vetlog.model.Pet;
+import com.josdem.vetlog.service.LocaleService;
 import com.josdem.vetlog.service.PetService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class GoogleMapController {
 
     private final PetService petService;
+    private final LocaleService localeService;
     private final GoogleProperties googleProperties;
     private final GeolocationProperties geolocationProperties;
 
     @GetMapping("/map")
-    public String showMap(@RequestParam("id") Long id, Model model) {
-        var pet = petService.getPetById(id);
-        var currentPetGeolocation = ApplicationCache.locations.get(pet.getId());
+    public String showMap(@RequestParam Optional<Long> id, Model model, HttpServletRequest request) {
+        log.info("Pet id: {}", id);
+        Location currentPetLocation = null;
+        Pet pet = null;
+        if(id.isPresent()){
+            log.info("Is present");
+            pet = petService.getPetById(id.get());
+            currentPetLocation = ApplicationCache.locations.get(pet.getId());
+            log.info("Current pet location: {}", currentPetLocation);
+        }
         var latitude =
-                currentPetGeolocation != null ? currentPetGeolocation.getLat() : geolocationProperties.getLatitude();
+                currentPetLocation != null ? currentPetLocation.getLat() : geolocationProperties.getLatitude();
         var longitude =
-                currentPetGeolocation != null ? currentPetGeolocation.getLng() : geolocationProperties.getLongitude();
+                currentPetLocation != null ? currentPetLocation.getLng() : geolocationProperties.getLongitude();
+        var message = currentPetLocation != null
+                ? pet.getName() + localeService.getMessage("map.pet.geolocation", request)
+                : localeService.getMessage("map.pet.default", request);
+        model.addAttribute("message", message);
         model.addAttribute("latitude", latitude);
         model.addAttribute("longitude", longitude);
         model.addAttribute("apiKey", googleProperties.getApiKey());
