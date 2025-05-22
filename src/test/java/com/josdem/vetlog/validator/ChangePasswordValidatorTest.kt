9c3095ab6cd03eb16034d4b7ad1943cc2842f9fd
Komplexe <artval570@gmail.com>
2025-05-22 18:48:31 +0300

    fixValidationTests

--- src/test/java/com/josdem/vetlog/validator/ChangePasswordValidatorTest.kt
+++ src/test/java/com/josdem/vetlog/validator/ChangePasswordValidatorTest.kt
@@ -1,26 +1,29 @@
 package com.josdem.vetlog.validator
 
 import com.josdem.vetlog.command.ChangePasswordCommand
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInfo
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
+import org.slf4j.LoggerFactory
 import org.springframework.validation.Errors
 
 internal class ChangePasswordValidatorTest {
+    companion object {
+        private val log = LoggerFactory.getLogger(ChangePasswordValidatorTest::class.java)
+    }
 
     private val validator = ChangePasswordValidator()
 
     @Test
-    @DisplayName("not validating since password does not match")
-    fun shouldRejectSincePasswordsAreNotEquals(testInfo: TestInfo) {
-        println("Running: ${testInfo.displayName}")
-        val command = ChangePasswordCommand().apply {
-            token = "token"
-            password = "password"
-            passwordConfirmation = "passwords"
-        }
+    fun `should reject since passwords are not equals`(testInfo: TestInfo) {
+        log.info("Running: ${testInfo.displayName}")
+        val command =
+            ChangePasswordCommand().apply {
+                token = "token"
+                password = "password"
+                passwordConfirmation = "passwords"
+            }
         val errors: Errors = mock()
 
         validator.validate(command, errors)
--- src/test/java/com/josdem/vetlog/validator/PetValidatorTest.kt
+++ src/test/java/com/josdem/vetlog/validator/PetValidatorTest.kt
@@ -1,47 +1,46 @@
 package com.josdem.vetlog.validator
 
 import com.josdem.vetlog.command.PetCommand
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInfo
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
+import org.slf4j.LoggerFactory
 import org.springframework.validation.Errors
 
 internal class PetValidatorTest {
+    companion object {
+        private val log = LoggerFactory.getLogger(PetValidatorTest::class.java)
+    }
 
     private val validator = PetValidator()
     private val errors: Errors = mock()
 
     @Test
-    @DisplayName("validating birthdate")
-    fun shouldValidateBirthdate(testInfo: TestInfo) {
-        println("Running: ${testInfo.displayName}")
+    fun `should validate birthdate`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
         val petCommand = getPetCommand("2021-01-17")
         validator.validate(petCommand, errors)
         verify(errors, never()).rejectValue(any(), any())
     }
 
     @Test
-    @DisplayName("validating empty birthdate")
-    fun shouldValidateEmptyBirthdate(testInfo: TestInfo) {
-        println("Running: ${testInfo.displayName}")
+    fun `should validate empty birthdate`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
         val petCommand = getPetCommand("")
         validator.validate(petCommand, errors)
         verify(errors, never()).rejectValue(any(), any())
     }
 
     @Test
-    @DisplayName("rejecting a birthdate")
-    fun shouldRejectBirthdate(testInfo: TestInfo) {
-        println("Running: ${testInfo.displayName}")
+    fun `should reject a birthdate`(testInfo: TestInfo) {
+        log.info(testInfo.displayName)
         val petCommand = getPetCommand("2026-01-17")
         validator.validate(petCommand, errors)
         verify(errors).rejectValue("birthDate", "pet.error.birthDate.past")
     }
 
-    private fun getPetCommand(birthdate: String): PetCommand =
-        PetCommand().apply { birthDate = birthdate }
+    private fun getPetCommand(birthdate: String): PetCommand = PetCommand().apply { birthDate = birthdate }
 }
