c235b6d6ae6b5697c9a6a765d15181535a5e9abb
josdem <joseluis.delacruz@gmail.com>
2025-06-05 06:50:23 -0600

    #593 Adding get location test ordered

--- src/main/java/com/josdem/vetlog/model/Location.java
+++ src/main/java/com/josdem/vetlog/model/Location.java
@@ -18,9 +18,11 @@ package com.josdem.vetlog.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Location {
     private double lat;
     private double lng;
