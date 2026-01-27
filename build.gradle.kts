d815f9147663b909d98add27bd84b8527ba69e05
josdem <joseluis.delacruz@gmail.com>
2026-01-27 14:48:13 -0600

    #785 Setting release version :)

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ val cglibVersion by extra("3.3.0")
 val mockitoKotlinVersion by extra("6.2.1")
 
 group = "com.josdem.vetlog"
-version = "3.3.12"
+version = "3.4.0"
 
 configurations {
   compileOnly {
