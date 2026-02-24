72a4ff235a6a9b969784eda281947d9fe2163e05
josdem <joseluis.delacruz@gmail.com>
2026-02-23 22:12:19 -0600

    #800 Updating Spring Boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 }
 
 plugins {
-  id("org.springframework.boot") version "4.0.2"
+  id("org.springframework.boot") version "4.0.3"
   id("io.spring.dependency-management") version "1.1.7"
   id("org.flywaydb.flyway") version "11.20.2"
   id("org.sonarqube") version "7.2.2.6593"
