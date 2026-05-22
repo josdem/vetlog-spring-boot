2f48e4bacda8b8cfbe5a5d2ed01eb30f01d7a780
khawaja-abdullah <khawaja.abdullah.ansar@gmail.com>
2026-05-22 22:38:26 +0500

    import and test fixes

--- src/main/java/com/josdem/vetlog/model/User.java
+++ src/main/java/com/josdem/vetlog/model/User.java
@@ -20,17 +20,16 @@ import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import com.josdem.vetlog.enums.Role;
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.io.Serializable;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
