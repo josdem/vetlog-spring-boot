b41f4c267e940e334852478d83b1d03a7c94597c
josdem <joseluis.delacruz@gmail.com>
2026-08-27 17:18:16 -0600

    Updating project version

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ val cglibVersion by extra("3.3.0")
 val mockitoKotlinVersion by extra("6.3.0")
 
 group = "com.josdem.vetlog"
-version = "3.8.3"
+version = "3.8.4"
 
 configurations {
   compileOnly {
